df59989d413714a4d3e8d5a956f903582c8d4de8
shanny <foncham.andin@yahoo.com>
2022-04-26 19:33:40 +0200

    input,button,usestate in the App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
 
-function App() {
+// Packages
+import {v4 as uuidv4} from 'uuid';
+
+//  Styles
+import "./App.css";
+
+
+/*
+  0. Import the necessary packages and/or hooks
+  1. Create the input and button Elements
+  2. Create the useStates
+  3. Send the input value to the single todo (onchange)
+  4. Send the todo to the todos (onsubmit)
+  5. 
+
+*/
+
+const App =() => {
+  // Create Single Value
+  // TEXT ONLY
+  const [todo,setTodo]= useState("");
+  function editTodo(e) {
+    setTodo(e.target.value);
+  }
+
+  // Add todo to All Todos
+  const [todos,setTodos]= useState([]);
+  
+  function onSubmit() {
+    // example todo {_id:"", todotext:""}
+    todos.push({_id:uuidv4(), todotext:todo})
+    console.log(todos)
+  }
+  console.log(todo);
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <> 
+      <h1>To Do List</h1>
+      <input type="text" name="todotext" id="todotext" placeholder="please insert task" 
+        onChange={(e)=>editTodo(e)}
+      >
+      </input>
+      <button 
+        onClick={()=>onSubmit()}
+      >Submit</button>
+     
+    </>
+  
   );
 }
 
